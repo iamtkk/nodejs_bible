6704c0ae24bb29a4d28aef671ed5f2507858d136
김태관 <iamtk@neowiz.com>
2024-01-23 23:04:08 +0900

    04-2 완료

--- 04-2-Exam/restFront.js
+++ 04-2-Exam/restFront.js
@@ -1,4 +1,60 @@
-document.getElementById('form').addEventListener('submit', (e) => {
+async function getUser() {
+  try {
+    const res = await axios.get("/users");
+    const users = res.data;
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+    Object.keys(users).map((key) => {
+      const userDiv = document.createElement("div");
+      const span = document.createElement("span");
+      const edit = document.createElement("button");
+      span.textContent = users[key];
+      edit.textContent = "수정";
+      edit.addEventListener("click", async () => {
+        const editName = prompt("수정할 이름을 입력해주세요.");
+        if (!editName) {
+          return alert("이름을 반드시 입력하여야 합니다.");
+        }
+        try {
+          await axios.put("/user/" + key, { name: editName });
+          getUser();
+        } catch (error) {
+          console.error(error);
+        }
+      });
+      const remove = document.createElement("button");
+      remove.textContent = "삭제";
+      remove.addEventListener("click", async () => {
+        try {
+          await axios.delete("/user/" + key);
+          getUser();
+        } catch (error) {
+          console.error(error);
+        }
+      });
+      userDiv.appendChild(span);
+      userDiv.appendChild(edit);
+      userDiv.appendChild(remove);
+      list.appendChild(userDiv);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+window.onload = getUser;
+
+document.getElementById("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  console.log(e.target.username);
+  const name = e.target.username.value;
+  if (!name) {
+    alert("이름을 입력해주세요.");
+  }
+  try {
+    await axios.post("/user", { name });
+    getUser();
+    e.target.username.value = "";
+  } catch (error) {
+    console.error(error);
+  }
 });
--- 04-2-Exam/restServer.js
+++ 04-2-Exam/restServer.js
@@ -1,19 +1,19 @@
-const http = require('http');
-const fs = require('fs').promises;
-const path = require('path');
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
 
 let users = {};
 
 const server = http.createServer(async (req, res) => {
   try {
-    if (req.method === 'GET') {
-      if (req.url === '/') {
-        const data = await fs.readFile(path.join(__dirname, 'restFront.html'));
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        const data = await fs.readFile(path.join(__dirname, "restFront.html"));
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         return res.end(data);
-      } else if (req.url === '/users') {
+      } else if (req.url === "/users") {
         res.writeHead(200, {
-          'Content-Type': 'application/json; charset=utf-8',
+          "Content-Type": "application/json; charset=utf-8",
         });
         return res.end(JSON.stringify(users));
       }
@@ -24,54 +24,53 @@ const server = http.createServer(async (req, res) => {
       } catch (error) {
         console.error(error);
       }
-    } else if (req.method === 'POST') {
-      if (req.url === '/user') {
-        let body = '';
-        req.on('data', (data) => {
+    } else if (req.method === "POST") {
+      if (req.url === "/user") {
+        let body = "";
+        req.on("data", (data) => {
           body += data;
         });
-        return req.on('end', () => {
+        return req.on("end", () => {
           const key = Date.now();
           users[key] = JSON.parse(body).name;
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-          return res.end('등록 성공');
+          res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+          return res.end("등록 성공");
         });
       }
-    } else if (req.method === 'PUT') {
-      if (req.url.startsWith('/user/')) {
-        const key = req.url.split('/')[2];
-        console.log(key);
-        let body = '';
-        req.on('data', (data) => {
+    } else if (req.method === "PUT") {
+      if (req.url.startsWith("/user/")) {
+        const key = req.url.split("/")[2];
+        let body = "";
+        req.on("data", (data) => {
           body += data;
         });
-        return req.on('end', () => {
+        return req.on("end", () => {
           users[key] = JSON.parse(body).name;
           res.writeHead(200, {
-            'Content-Type': 'application/json; charset=utf-8',
+            "Content-Type": "application/json; charset=utf-8",
           });
           return res.end(JSON.stringify(users));
         });
       }
-    } else if (req.method === 'DELETE') {
-      if (req.url.startsWith('/user/')) {
-        const key = req.url.split('/')[2];
+    } else if (req.method === "DELETE") {
+      if (req.url.startsWith("/user/")) {
+        const key = req.url.split("/")[2];
         delete users[key];
         res.writeHead(200, {
-          'Content-Type': 'application/json; charset=utf-8',
+          "Content-Type": "application/json; charset=utf-8",
         });
         return res.end(JSON.stringify(users));
       }
     }
-    res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
-    return res.end('NOT FOUND');
+    res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+    return res.end("NOT FOUND");
   } catch (error) {
     console.error(error);
-    res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
     res.end(err.message);
   }
 });
 
 server.listen(8080, () => {
-  console.log('8080 포트로 리스닝 되었습니다.');
+  console.log("8080 포트로 리스닝 되었습니다.");
 });
